75be5df125961cd749ac96c5491887f057445041
Saumya <usam9010@gmail.com>
2023-05-27 16:02:17 +0530

    download page bug fixed

--- server/server.js
+++ server/server.js
@@ -1,21 +1,22 @@
-const express = require("express");
-const {connectDB} = require('./config/db');
+const express = require('express');
+const { connectDB } = require('./config/db');
 const app = express();
 const fileRouter = require('./routes/filesRoutes');
-const path =require('path')
+const path = require('path');
 const showRouter = require('./routes/showRoutes');
 const PORT = process.env.PORT || 3000;
 connectDB();
 
+app.use(express.static('public'));
+
 //Template engine
 app.set('views', path.join(__dirname, '/views'));
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 //Routes
-app.use('/api/files',fileRouter);
-app.use('/api/files',showRouter);
+app.use('/api/files', fileRouter);
+app.use('/api/files', showRouter);
 
 app.listen(PORT, () => {
   console.log(`server running on port : ${PORT}`);
 });
-  
